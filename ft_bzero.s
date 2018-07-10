8668a509d2c63386511d45a18264e0fa90f81286
yguaye <yguaye@student.42.fr>
2018-07-10 08:07:03 +0200

    ft_bzero

---
+++ ft_bzero.s
@@ -0,0 +1,13 @@
+%include			"macros.s"
+
+					global		FUNC(ft_bzero)	; void ft_bzero(void *s, size_t n)
+
+					section		.text
+FUNC(ft_bzero):		mov			r8, rsi			; set r8 to n	
+					cmp			r8, 0
+					je			done
+zero:				mov			byte [rdi], 0
+					inc			rdi
+					dec			r8
+					jnz			zero
+done:				ret
