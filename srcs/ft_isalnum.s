21d45937ae71e5d1b649e23aeff749ad0f1969f8
Yanis Guaye <yguaye@student.42.fr>
2018-07-11 20:45:32 +0200

    Change ft_isalnum to respect the calling convention

--- srcs/ft_isalnum.s
+++ srcs/ft_isalnum.s
@@ -5,12 +5,12 @@
 ; int ft_isalnum(int c);
 					section		.text
 FUNC(ft_isalnum):	mov			ecx, edi
-					xor			ebx, ebx
+					xor			edx, edx
 					is_inside	ecx, '0', '9'
-					setbe		bl				; stores result of '0' >= c && c <= '9'
+					setbe		dl				; stores result of '0' >= c && c <= '9'
 					and			edi, 0xffffffdf	; Uppercases c by unsetting the 6th bit.
 					xor			eax, eax		; sets return value to 0
 					is_inside	edi, 'A', 'Z'
 					setbe		al				; sets eax if 'A' >= c && c <= 'Z'
-					or			al, bl
+					or			al, dl
 					ret
