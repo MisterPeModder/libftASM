304ebb1c1fca5614a67107849261fcee042933ab
Yanis Guaye <yguaye@student.42.fr>
2018-07-12 15:33:30 +0200

    Add ft_puts

--- includes/libfts.h
+++ includes/libfts.h
@@ -6,7 +6,7 @@
 /*   By: yguaye <yguaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/12 05:34:53 by yguaye            #+#    #+#             */
-/*   Updated: 2018/07/12 14:30:23 by yguaye           ###   ########.fr       */
+/*   Updated: 2018/07/12 15:37:19 by yguaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int					ft_isalnum(int c);
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isprint(int c);
+int					ft_puts(const char *str);
 char				*ft_strcat(char *dst, const char *src);
 size_t				ft_strlen(const char *str);
 int					ft_tolower(int c);
---
+++ srcs/ft_puts.s
@@ -0,0 +1,33 @@
+%include			"macros.s"
+
+					global		FUNC(ft_puts)
+					extern		FUNC(ft_strlen)
+
+; int ft_puts(const char *str);
+					section		.text
+FUNC(ft_puts):		push		rbp									; save callee-save register
+					mov			rbp, rsp
+					push		rdi
+					call		FUNC(ft_strlen)
+					mov			rdx, rax							; set write's size parameter
+					pop			rsi									; write 'str'
+					mov			rsp, rbp
+					pop			rbp									; restore callee-save register
+					mov			rdi, 1								; write to stdout
+					mov			rax, WRAP_SYSCALL(SYSCALL_EXIT_ID)	; set syscall to write
+					syscall											; write(1, str, ft_strlen(str));
+					cmp			rax, -4095
+					jae			FUNC(ft_puts_err)
+					mov			rdx, 1								; write 1 char
+					lea			rsi, [rel newline]					; write end-of-line character
+					mov			rax, WRAP_SYSCALL(SYSCALL_EXIT_ID)	; set syscall to write
+					syscall											; write(1, '\n', 1);
+					cmp			rax, -4095
+					jae			FUNC(ft_puts_err)
+					xor			rax, rax
+					ret												; return (0);
+FUNC(ft_puts_err):	mov			rax, -1								; return EOF (-1) on error
+					ret
+
+					section		.data
+newline:			db			10
