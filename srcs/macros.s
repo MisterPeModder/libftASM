f2be2e76a8089e62f3317148bf0ef5ecca2a16a7
yguaye <yguaye@student.42.fr>
2018-07-14 06:09:05 +0200

    Remove unused macros/defines

--- srcs/macros.s
+++ srcs/macros.s
@@ -24,44 +24,6 @@
 	%define SYSCALL_WRITE_ID	1
 %endif
 
-
-; Generic syscall macro, takes the syscall id as the first parameters.
-; note: to stay compatible with linux, sys_generic cannot take more than 7 params.
-%macro	sys_generic	1-*
-		mov			rax, %1
-	%if %0 >= 2
-		mov			rdi, %2
-	%endif
-	%if %0 >= 3
-		mov			rsi, %3
-	%endif
-	%if %0 >= 4
-		mov			rdx, %4
-	%endif
-	%if %0 >= 5
-		mov			rcx, %5
-	%endif
-	%if %0 >= 6
-		mov			r8, %6
-	%endif
-	%if %0 = 7
-		mov			r9, %7
-	%endif
-	%if %0 > 7
-		%error "sys_generic can't take more than 7 arguments"
-	%endif
-		syscall
-%endmacro
-
-; More useful syscalls
-%macro 	sys_exit	1
-		sys_generic SYSCALL_EXIT_ID, %1
-%endmacro
-
-%macro	sys_write	3
-		sys_generic	SYSCALL_WRITE_ID, %1, %2, %3
-%endmacro
-
 ; limits
 %define I64_MAX 0xffffffffffffffff
 
