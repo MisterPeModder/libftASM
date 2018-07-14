16c14e23f757441aef40347afa77ebbd4b63939d
yguaye <yguaye@student.42.fr>
2018-07-14 05:57:05 +0200

    Add dummy main.c file

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <libfts.h>
+
+int					main(void)
+{
+	ft_puts("[EN]: Consider using the test program "
+			"instead of this crappy main ;)");
+	ft_puts("[FR]: Je vous conseille d'utiliser le programme de test"
+			"plutôt que ce main pourri ;)");
+	return (42);
+}
